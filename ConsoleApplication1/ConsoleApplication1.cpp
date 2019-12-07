c3dac854c9285c5ba2f663a3e379c466cb683094
whwo2142@naver.com <whwo2142@naver.com>
2019-12-07 18:27:48 +0900

    e

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,13 +1,15 @@
 ﻿#include <stdio.h>
-#define MAX 9
 
 int main(void)
 {
-	
-	printf("==== 구구단 출력 ===\n");
-	for (int i = 2; i <= MAX; i++)
-	{
-		printf("%6d단 출력\n", i);
+	int data[] = { 3,21,35,57,24,82,8 };
+
+	for (int i = 0; i < sizeof(data) / sizeof(int); i++) {
+		*(data + i) += 10;
+		printf("%3d", *(data + i));
 	}
-	return 0;
-}xs
+
+	puts("   ");
+
+	return 0; 
+}
